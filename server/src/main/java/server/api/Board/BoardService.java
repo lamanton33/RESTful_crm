e8de90caf145cc97b830a6380cecc7e958d3e0c2
Iurii Poţeluev <i.poteluev-1@student.tudelft.nl>
2023-03-21 17:42:57 +0100

    Created BoardController and BoardService and implemented Delete request and put request for boards

---
+++ server/src/main/java/server/api/Board/BoardService.java
@@ -0,0 +1,62 @@
+package server.api.Board;
+
+import commons.Board;
+import commons.Card;
+import commons.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import server.database.BoardRepository;
+
+import java.util.List;
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+
+
+    /**
+     * Retrieves all the Boards from the repository
+     */
+    public Result<List<Board>> getAllBoards(){
+        try {
+            return Result.SUCCESS.of(boardRepository.findAll());
+        }catch (Exception e){
+            return Result.FAILED_GET_ALL_BOARDS;
+        }
+    }
+//
+//    /**
+//     * Adds the Board to the repository
+//     * @param board
+//     */
+//    public Result<Board> addNewBoard (Board board){
+//        if (board == null || board.boardTitle == null) {
+//            return Result.OBJECT_ISNULL.of(null);
+//        }
+//        try {
+//            return Result.SUCCESS.of(boardRepository.save(board));
+//        }catch (Exception e){
+//            return Result.FAILED_ADD_NEW_BOARD.of(null);
+//        }
+//    }
+
+    /**
+     * Deletes the Board with the given id
+     * @param id
+     */
+    public Result<Object> deleteBoard (Integer id) {
+        try {
+            boardRepository.deleteById(id);
+            return Result.SUCCESS;
+        } catch (Exception e){
+            return Result.FAILED_DELETE_BOARD.of(null);
+        }
+    }
+
+
+}
